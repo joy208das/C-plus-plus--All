ac747c38d184b17dfe7215ed8ce0cac8a104683b
joy208das <158313870+joy208das@users.noreply.github.com>
2024-06-15 02:28:29 +0600

    Add files via upload

---
+++ 09and-or.cpp
@@ -0,0 +1,14 @@
+// tell me if a number has three digit or not
+#include<iostream>
+using namespace std;
+
+int main(){
+int n;
+cout<<"enter number:";
+cin>>n;
+
+if(n>99 and n<1000) cout<<"Yes,it has 3 digit \n";
+else cout<<"NO";
+
+
+}
